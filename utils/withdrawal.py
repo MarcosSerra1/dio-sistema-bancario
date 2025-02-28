764ef5bc8246d2071ae1c3f770224f43319e352e
Marcos Serra <marcosserra133@gmail.com>
2025-02-28 17:47:41 -0300

    feat: ✨ Melhora a documentação da função de saque com detalhes sobre parâmetros e comportamento

--- utils/withdrawal.py
+++ utils/withdrawal.py
@@ -1,40 +1,35 @@
 import locale
 
-
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-def withdrawal_function(withdrawal_value, account_balance = 0, limit_withdrawal = 500, transactions=[]):
-    '''
-    Função para realizar saque na conta do usuário
-    :param withdrawal_value: Valor do saque
-    :param account_balance: Saldo da conta do usuário
-    :return: Novo saldo da conta e mensagem de sucesso ou erro
-    '''
+def withdrawal_function(withdrawal_value, account_balance=0, limit_withdrawal=500, transactions=[]):
+    """
+    Função para realizar saque na conta do usuário.
+
+    Esta função recebe um valor de saque, o saldo atual da conta, o limite de saque e a lista de transações,
+    valida o valor do saque, atualiza o saldo da conta e registra a transação na lista de transações.
+
+    :param withdrawal_value: Valor do saque (pode ser uma string ou um número).
+    :param account_balance: Saldo atual da conta do usuário (padrão é 0).
+    :param limit_withdrawal: Limite máximo permitido para o saque (padrão é 500).
+    :param transactions: Lista de transações realizadas na conta.
+    :return: Novo saldo da conta e mensagem de sucesso ou erro.
+    """
     try:
         withdrawal_value = float(withdrawal_value)
         account_balance = float(account_balance)
 
         if withdrawal_value <= account_balance:
-            if  withdrawal_value > 0 and withdrawal_value <= limit_withdrawal:
+            if withdrawal_value > 0 and withdrawal_value <= limit_withdrawal:
                 account_balance -= withdrawal_value
                 transactions.append(f'Saque de {locale.currency(withdrawal_value, grouping=True)}')
                 message = f'Saque de {locale.currency(withdrawal_value, grouping=True)} realizado com sucesso! Seu novo saldo é de {locale.currency(account_balance, grouping=True)}'
                 return account_balance, message
             else:
-                return account_balance, 'Valor do saque deve ser maior que zero e menor ou igual a R$ 500,00'
-
+                return account_balance, f'Valor do saque deve ser maior que zero e menor ou igual a {locale.currency(limit_withdrawal, grouping=True)}'
         else:
             return account_balance, f'Saldo insuficiente para realizar o saque de {locale.currency(withdrawal_value, grouping=True)}. Saldo atual: {locale.currency(account_balance, grouping=True)}'
-
     except ValueError:
         return account_balance, 'Valor do saque deve ser um número!'
     except Exception as error:
         return account_balance, f'Erro ao realizar saque: {error}'
-
-
-# Teste da função
-if __name__ == '__main__':
-    saque = withdrawal_function(500, 500)
-    print(saque)
-
-# só posso realalizar 3 saques diarios de até R$ 500,00
