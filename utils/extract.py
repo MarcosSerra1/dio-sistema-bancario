ee1a7022f56ee6379ddb3304335b4022b7350b1d
Marcos Serra <marcosserra133@gmail.com>
2025-02-28 17:47:35 -0300

    feat: ✨ Atualiza a documentação da função de extrato com detalhes sobre parâmetros e comportamento

--- utils/extract.py
+++ utils/extract.py
@@ -3,12 +3,16 @@ import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF8')
 
 def extract_function(account_balance, transactions):
-    '''
-    Função para realizar extrato da conta do usuário
-    :param account_balance: Saldo da conta do usuário
-    :param transactions: Lista de transações realizadas
-    :return: Mensagem com o saldo da conta e histórico de transações
-    '''
+    """
+    Função para gerar o extrato da conta do usuário.
+
+    Esta função recebe o saldo atual da conta e a lista de transações realizadas,
+    formata o saldo e o histórico de transações, e retorna uma mensagem com essas informações.
+
+    :param account_balance: Saldo da conta do usuário.
+    :param transactions: Lista de transações realizadas na conta.
+    :return: Mensagem com o saldo da conta e histórico de transações.
+    """
     try:
         account_balance = float(account_balance)
         extract_message = f'Seu saldo é de {locale.currency(account_balance, grouping=True)}\n'
